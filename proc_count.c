183219ea06aa466b074c7cfd963bfac749b7efb5
Nicole Ju <nicoleju03@gmail.com>
2023-01-13 17:49:56 -0800

    modified proc_count.c

--- proc_count.c
+++ proc_count.c
@@ -4,20 +4,37 @@
 #include <linux/seq_file.h>
 #include <linux/sched.h>
 
+static struct proc_dir_entry *entry;
+
+static int count_processes(struct seq_file *m, void *v)
+{
+  int i=0;
+  struct task_struct *p;
+  for_each_process(p)
+    {
+      i++; 
+    }
+  seq_printf(m, "%d\n", i);
+  return 0;
+}
+
+
 static int __init proc_count_init(void)
 {
-	pr_info("proc_count: init\n");
-	return 0;
+  entry = proc_create_single("count",0,NULL,count_processes);
+  pr_info("proc_count: init\n");
+  return 0;
 }
 
 static void __exit proc_count_exit(void)
 {
-	pr_info("proc_count: exit\n");
+  proc_remove(entry);
+  pr_info("proc_count: exit\n");
 }
-
 module_init(proc_count_init);
 module_exit(proc_count_exit);
 
-MODULE_AUTHOR("Your name");
-MODULE_DESCRIPTION("One sentence description");
+
+MODULE_AUTHOR("Nicole Ju");
+MODULE_DESCRIPTION("count number of processes on machine");
 MODULE_LICENSE("GPL");
